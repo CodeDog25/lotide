8cc61b4a489d9a55706f012c8ef5b0bf291100ee
Sicily Ding <xishading@gmail.com>
2022-12-08 00:46:44 -0700

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-      }
-};
-
 const eqArrays = function(array1, array2) {
    for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -20,4 +12,4 @@ const eqArrays = function(array1, array2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
