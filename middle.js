a475933602aad7972b44302ded3539b8b480e0f1
Sicily Ding <xishading@gmail.com>
2022-12-08 01:11:47 -0700

    Refactored middle

--- middle.js
+++ middle.js
@@ -1,22 +1,6 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-      } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-      }
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
 
-const eqArrays = function(array1, array2) {
-   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-        return false;
-    } 
-   }
-    if (array1.length !== array2.length) {
-      return false;
-    }
-    return true;
-};
 
 const middle = function(arr) {
     const half = arr.length >> 1;
@@ -24,7 +8,5 @@ const middle = function(arr) {
     return arr.slice(half - offset, half + 1);
 };
 
+module.exports = middle;
 
-
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6])); 
