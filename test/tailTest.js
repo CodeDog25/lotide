d2880620f5dc862c2dfd45bccc4b853c3e6ec9c4
Sicily Ding <xishading@gmail.com>
2022-12-09 01:03:02 -0700

    refactored tailTest.js with Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#tail", () => {
+    it("returns [ 6,7 ] for [5,6,7]", () => {
+      assert.deepEqual(tail([5,6,7]), 5);
+    });
+    it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+      });
+  });
